c9edbf4364e7b1c16d49a4379cdc159858303b26
Liliana Torrico <lilitatd@gmail.com>
2018-11-26 23:52:51 -0400

    Planeaciones fueron creadas

---
+++ app/Http/Controllers/PlaneacionController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace SilverDC\Http\Controllers;
+
+use SilverDC\Planeacion;
+use Illuminate\Http\Request;
+
+class PlaneacionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        //return ('test');
+        return view('planeaciones.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        /* 
+            $table->string('nombre', 20);   
+            $table->date('fecha');
+            $table->float('avanceTotal', 4, 2);
+            $table->float('avancePorDia', 4, 2);
+            $table->integer('diasTrabajo');
+            $table->integer('gestion');
+            $table->string('mes', 11);
+            $table->string('est', 15);
+            $table->string('estado', 10);
+        */            
+        $planeacion = new Planeacion();
+        $planeacion->nombre = $request->input('nombre');
+        $planeacion->fecha = $request->input('fecha');
+        $planeacion->avanceTotal = $request->input('avanceTotal');
+        $planeacion->avancePorDia = $request->input('avancePorDia');
+        $planeacion->diasTrabajo = $request->input('diasTrabajo');
+        $planeacion->gestion = $request->input('gestion');
+        $planeacion->mes = $request->input('mes');
+        $planeacion->est = $request->input('est');
+        $planeacion->estado = 'Pendiente';
+        $planeacion->save();
+        return 'planeacion guardada';
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ database/migrations/2018_11_26_205335_create_planeacions_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePlaneacionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('planeacions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre', 20);
+            $table->date('fecha');
+            $table->float('avanceTotal', 4, 2);
+            $table->float('avancePorDia', 4, 2);
+            $table->integer('diasTrabajo');
+            $table->integer('gestion');
+            $table->string('mes', 11);
+            $table->string('est', 15);
+            $table->string('estado', 10);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('planeacions');
+    }
+}
